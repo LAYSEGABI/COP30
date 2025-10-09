6c1d2420a6a60e3bc72592d10f5cad72ee4c3387
LAYSEGABI <laysegabrielly13@gmail.com>
2025-10-09 14:35:28 -0300

    mudando o mobile back do hero como img

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,20 +1,19 @@
 import "../styles/Hero.css";
 import backVideo from '../assets/back.mp4';
+import backImage from '../assets/back-mobile.png'; // imagem para mobile
 import translations from "../lang/translations";
 
-function HeroSection({ lang}) {
+function HeroSection({ lang }) {
   return (
     <section className="hero">
-      <video autoPlay muted loop playsInline>
+      <video className="hero-video" autoPlay muted loop playsInline>
         <source src={backVideo} type="video/mp4" />
       </video>
+      <img className="hero-image" src={backImage} alt="Fundo hero" />
+      
       <div className="container">
-        <h1>
-          {translations[lang].hero_juventude_titulo}
-        </h1>
-        <p>
-          {translations[lang].hero_juventude_desc}
-        </p>
+        <h1>{translations[lang].hero_juventude_titulo}</h1>
+        <p>{translations[lang].hero_juventude_desc}</p>
         <div className="CTAs">
           <a href="#sobre-nos">{translations[lang].hero_juventude_btn_1}</a>
           <a href="#cop30">{translations[lang].hero_juventude_btn_2}</a>
